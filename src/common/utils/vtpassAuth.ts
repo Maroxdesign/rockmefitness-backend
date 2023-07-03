697976706c49a8055ea42d799aa4af99aec9f39a
jc-coder <josephchimezie2003@gmail.com>
2023-07-03 22:28:51 +0100

    update codebase

--- src/common/utils/vtpassAuth.ts
+++
@@ -1,6 +0,0 @@
-import { environment } from '../config/environment';
-
-export const vtpassAuth = () => {
-  const token = `${environment.VT_PASS.USERNAME}:${environment.VT_PASS.PASSWORD}`;
-  return Buffer.from(token).toString('base64');
-};
